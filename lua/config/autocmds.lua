4428d807c975e3211daad1e0acd9e8e1f5d62deb
ocsiker <qthang89vn@gmail.com>
2023-10-26 02:25:26 +0700

    feat:configure for java debug working well with maven

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,18 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("Commentary_ft"),
+  pattern = { "sql", "mysql" },
+  command = [[setlocal commentstring=--\ %s]]
+})
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
+  group = augroup("sql_ft"),
+  pattern = { "mysql" },
+  command = [[set filetype=sql]]
+})
---
+++ lua/plugins/disable.lua
@@ -0,0 +1,11 @@
+return {
+  -- disable mini.surround
+  { "echasnovski/mini.surround", enabled = false },
+  --disable mini.comment
+  { "echasnovski/mini.comment", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "github/copilot.vim", enabled = false },
+  -- { "tpope/vim-commentary",      enabled = false },
+  -- { "nvim-telescope/telescope.nvim", enabled = false },
+  -- { "ggandor/leap.nvim", enabled = false },
+}
