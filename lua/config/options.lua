0cc6df8ea2e42c50a950da74d7075c5557504f9c
ocsiker <qthang89vn@gmail.com>
2024-01-12 04:59:27 +0700

    fix: change format in keymaps.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-vim.opt.nrformats:append("alpha")
+-- vim.opt.nrformats:append("alpha")
 vim.opt.tabstop = 4
